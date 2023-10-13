c248584e97673f7385c6698e1ce1b8b1b3c93d40
Rajat <rajatprajapati09@gmail.com>
2023-10-14 03:55:47 +0530

    [emplyee Wage Program]

--- Main.java
+++ Main.java
@@ -3,36 +3,46 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int wagePerHour=20;
-        int fullDayHour=8;
-        int halfDayHour=4;
+        int totalWorkingHour=0;
+       int totalWorkingDays=0;
+       final int wagePerHour=20;
+       final int fullDayHour=8;
+       final int halfDayHour=4;
         int Wage= 0;
         int halfDayCount=0;
         int fullDayCount=0;
         int absent=0;
 
+
         Random rnd= new Random();
 
         System.out.println("Welcome to Employee wage program");
 
-        for(int i=0;i<20;i++)
+        while (totalWorkingDays!=20&&totalWorkingHour<=100)
         {
-            int a=rnd.nextInt(3);
 
+          totalWorkingDays++;
+            int a=rnd.nextInt(3);
             switch(a)
             {
                 case 0: absent++	;
                     break;
 
                 case 1: halfDayCount++	;
+                    totalWorkingHour=totalWorkingHour+halfDayHour;
                     break;
 
                 default: fullDayCount++	;
-                    break;
+                    totalWorkingHour=totalWorkingHour+fullDayHour;
 
+                    break;
             }
         }
         System.out.println("Total wage for 20 days ----"+(Wage=((fullDayCount*fullDayHour)+(halfDayCount*halfDayHour))*wagePerHour)+"\n absent : "+absent+"\n halfDay : "+halfDayCount+"\n fullDay : "+fullDayCount);
+        System.out.println("total hours in 20 days ----"+(totalWorkingHour));
 
     }
-}
+
+
+
+    }
